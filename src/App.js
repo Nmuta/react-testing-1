8f86dfc37a82ddb43ae03c26590893ae4a89a8d9
Tannen <ch.hillebrand@googlemail.com>
2021-03-30 14:43:12 +0200

    tidy up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Expenses from './components/Expenses';
 
-const expenses = [
-  { date: '2017-04-05', amount: 0 },
-  { date: '2017-04-05', amount: 4 },
-]
 function App() {
 
 
   return (
     <div className="App">
-    <Expenses expenses = {expenses}/>
 
     </div>
   );
